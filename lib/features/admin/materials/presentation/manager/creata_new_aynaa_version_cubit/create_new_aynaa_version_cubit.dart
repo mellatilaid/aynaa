aa44f1d1aa9d82736970b6094bdf3c89a02d5d07
mellati laid <laidmellati@gmail.com>
2024-11-27 00:28:40 -0800

    create new aynaa version cubit done

--- lib/features/admin/materials/presentation/manager/creata_new_aynaa_version_cubit/create_new_aynaa_version_cubit.dart
+++ lib/features/admin/materials/presentation/manager/creata_new_aynaa_version_cubit/create_new_aynaa_version_cubit.dart
@@ -1,8 +1,23 @@
+import 'package:atm_app/features/admin/materials/domain/repos/materials_repo.dart';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'create_new_aynaa_version_state.dart';
 
 class CreateNewAynaaVersionCubit extends Cubit<CreateNewAynaaVersionState> {
-  CreateNewAynaaVersionCubit() : super(CreateNewAynaaVersionInitial());
+  final MaterialsRepo materialsRepo;
+  CreateNewAynaaVersionCubit(this.materialsRepo)
+      : super(CreateNewAynaaVersionInitial());
+
+  Future<void> createNewAynaaVersion({required String versionName}) async {
+    emit(CreateNewAynaaVersionLoading());
+    final resault =
+        await materialsRepo.setAynaaVersion(versionName: versionName);
+
+    resault.fold((failure) {
+      emit(CreateNewAynaaVersionFailure(errMessage: failure.errMessage));
+    }, (succuss) {
+      emit(CreateNewAynaaVersionSuccess());
+    });
+  }
 }
