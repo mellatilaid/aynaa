379020f618c2fbc8ac23b3c273eec5951cbc017d
mellati laid <laidmellati@gmail.com>
2025-01-14 01:34:24 -0800

    add new subject cubit done

--- lib/features/admin/materials/presentation/manager/add_new_subject_cubit/add_new_subject_cubit.dart
+++ lib/features/admin/materials/presentation/manager/add_new_subject_cubit/add_new_subject_cubit.dart
@@ -1,8 +1,21 @@
+import 'package:atm_app/features/admin/materials/domain/repos/subjects_repo.dart';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+
+import '../../../domain/entities/subjects_entity.dart';
 
 part 'add_new_subject_state.dart';
 
 class AddNewSubjectCubit extends Cubit<AddNewSubjectState> {
-  AddNewSubjectCubit() : super(AddNewSubjectInitial());
+  final SubjectsRepo subjectsRepo;
+  AddNewSubjectCubit({required this.subjectsRepo})
+      : super(AddNewSubjectInitial());
+
+  Future<void> addNewSubject({required SubjectsEntity subject}) async {
+    emit(AddNewSubjectLoading());
+    final result = await subjectsRepo.addSubject(subject: subject);
+    result.fold(
+        (failure) => emit(AddNewSubjectFailure(errMessage: failure.errMessage)),
+        (sucuss) => emit(AddNewSubjectSuccuss()));
+  }
 }
