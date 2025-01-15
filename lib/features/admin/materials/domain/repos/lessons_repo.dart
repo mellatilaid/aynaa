ecdc6dd79fa79dc02d2b24bbd4272d3684ad64c6
mellati laid <laidmellati@gmail.com>
2025-01-15 03:01:54 -0800

    created lessons repo

---
+++ lib/features/admin/materials/domain/repos/lessons_repo.dart
@@ -0,0 +1,16 @@
+import 'package:atm_app/core/errors/failures.dart';
+import 'package:atm_app/features/admin/materials/domain/entities/lesson_entity.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class LessonsRepo {
+  Future<Either<Failures, String>> saveLesson({required LessonEntity lesson});
+
+  Future<Either<Failures, List<LessonEntity>>> fetchLessons(
+      {required String subjectID});
+  Future<Either<Failures, void>> addLesson({required LessonEntity lesson});
+
+  Future<Either<Failures, void>> deleteLesson({required String lessonID});
+
+  Future<Either<Failures, void>> updateLesson(
+      {required String lessonID, required Map<String, dynamic> data});
+}
