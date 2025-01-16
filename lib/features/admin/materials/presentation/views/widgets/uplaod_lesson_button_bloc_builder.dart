cdfde238abc255ef183d50a25a63c5a8f9ee2bd2
mellati laid <laidmellati@gmail.com>
2025-01-16 04:28:18 -0800

    refacotored upload lesson media button section widget

---
+++ lib/features/admin/materials/presentation/views/widgets/uplaod_lesson_button_bloc_builder.dart
@@ -0,0 +1,44 @@
+import 'package:atm_app/const.dart';
+import 'package:atm_app/core/widgets/custom_action_button_type2.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../../domain/entities/lesson_entity.dart';
+import '../../manager/add_lesson_cubit/add_lesson_cubit.dart';
+
+class UploadLessonButtonBuilder extends StatelessWidget {
+  final TextEditingController lessonContent;
+  const UploadLessonButtonBuilder({super.key, required this.lessonContent});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<AddLessonCubit, AddLessonState>(
+      builder: (context, state) {
+        return CustomActionButtonType2(
+            isLoading: state is AddLessonLoading ? true : false,
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            onPressed: () {
+              final addLessonBloc = context.read<AddLessonCubit>();
+              final lesson = _toLessonEnitiy(addLessonCubit: addLessonBloc);
+              BlocProvider.of<AddLessonCubit>(context)
+                  .addLesson(lesson: lesson);
+            },
+            backGroundColor: kPrimaryColor,
+            icon: const FaIcon(
+              FontAwesomeIcons.upload,
+              color: Colors.white,
+            ),
+            title: 'تحميل');
+      },
+    );
+  }
+
+  _toLessonEnitiy({required AddLessonCubit addLessonCubit}) {
+    return LessonEntity(
+      description: lessonContent.text,
+      aynaaVersionId: addLessonCubit.versionID!,
+      subjectId: addLessonCubit.subjectID!,
+    );
+  }
+}
--- lib/features/admin/materials/presentation/views/widgets/upload_lesson_media_button_section.dart
+++ lib/features/admin/materials/presentation/views/widgets/upload_lesson_media_button_section.dart
@@ -1,15 +1,7 @@
-import 'dart:developer';
-
-import 'package:atm_app/const.dart';
-import 'package:atm_app/core/widgets/custom_action_button_type2.dart';
-import 'package:atm_app/features/admin/materials/domain/entities/lesson_entity.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:go_router/go_router.dart';
 
 import '../../../../../../core/widgets/custom_rounded_button_with_title.dart';
-import '../../manager/add_lesson_cubit/add_lesson_cubit.dart';
+import 'uplaod_lesson_button_bloc_builder.dart';
 
 class UplaodLessonMediaButtonSection extends StatefulWidget {
   final TextEditingController lessonContent;
@@ -31,26 +23,7 @@ class _UplaodLessonMediaButtonSectionState
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
       child: isMediaUpladed
-          ? BlocBuilder<AddLessonCubit, AddLessonState>(
-              builder: (context, state) {
-                if (state is AddLessonSuccuss) {
-                  context.pop();
-                }
-                return CustomActionButtonType2(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    onPressed: () {
-                      final lesson = toLessonEnitiy();
-                      BlocProvider.of<AddLessonCubit>(context)
-                          .addLesson(lesson: lesson);
-                    },
-                    backGroundColor: kPrimaryColor,
-                    icon: const FaIcon(
-                      FontAwesomeIcons.upload,
-                      color: Colors.white,
-                    ),
-                    title: 'تحميل');
-              },
-            )
+          ? UploadLessonButtonBuilder(lessonContent: widget.lessonContent)
           : Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -65,24 +38,6 @@ class _UplaodLessonMediaButtonSectionState
                 ),
               ],
             ),
-    );
-  }
-
-  toLessonEnitiy() {
-    final addLessonBloc = context.read<AddLessonCubit>();
-    log(addLessonBloc.versionID ?? 'version id is null');
-    if (addLessonBloc.versionID == null || addLessonBloc.subjectID == null) {
-      log('null');
-      return LessonEntity(
-        description: widget.lessonContent.text,
-        aynaaVersionId: '',
-        subjectId: '',
-      );
-    }
-    return LessonEntity(
-      description: widget.lessonContent.text,
-      aynaaVersionId: addLessonBloc.versionID!,
-      subjectId: addLessonBloc.subjectID!,
     );
   }
 }
