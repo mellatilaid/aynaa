59d2a28a66dc6c649f2ec0767f105716c1da0189
mellati laid <laidmellati@gmail.com>
2025-01-15 23:58:36 -0800

    add lesson cubit done

--- lib/features/admin/materials/presentation/manager/add_lesson_cubit/add_lesson_cubit.dart
+++ lib/features/admin/materials/presentation/manager/add_lesson_cubit/add_lesson_cubit.dart
@@ -1,8 +1,22 @@
+import 'package:atm_app/features/admin/materials/domain/repos/lessons_repo.dart';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+
+import '../../../domain/entities/lesson_entity.dart';
 
 part 'add_lesson_state.dart';
 
 class AddLessonCubit extends Cubit<AddLessonState> {
-  AddLessonCubit() : super(AddLessonInitial());
+  final LessonsRepo lessonsRepo;
+  AddLessonCubit(this.lessonsRepo) : super(AddLessonInitial());
+
+  Future<void> addLesson({required LessonEntity lesson}) async {
+    emit(AddLessonLoading());
+    final result = await lessonsRepo.addLesson(lesson: lesson);
+    result.fold((failure) {
+      emit(AddLessonFailure(errMessage: failure.errMessage));
+    }, (success) {
+      emit(AddLessonSuccuss());
+    });
+  }
 }
