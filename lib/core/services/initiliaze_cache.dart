726d949910a52b6e4563e253a1aad0aee547f7ba
mellati laid <laidmellati@gmail.com>
2025-01-26 05:21:10 -0800

    regestered lesson entity adaptor

--- lib/core/services/initiliaze_cache.dart
+++ lib/core/services/initiliaze_cache.dart
@@ -1,6 +1,7 @@
 import 'package:atm_app/features/admin/materials/domain/entities/aynaa_versions_entity.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
+import '../../features/admin/materials/domain/entities/lesson_entity.dart';
 import '../../features/admin/materials/domain/entities/subjects_entity.dart';
 
 Future<void> initializeCache() async {
@@ -9,4 +10,5 @@ Future<void> initializeCache() async {
   // Register all adapters
   Hive.registerAdapter(AynaaVersionsEntityAdapter());
   Hive.registerAdapter(SubjectsEntityAdapter());
+  Hive.registerAdapter(LessonEntityAdapter());
 }
