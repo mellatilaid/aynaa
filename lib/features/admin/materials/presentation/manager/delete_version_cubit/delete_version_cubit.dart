f474b51dfe52fb2134a9ccc14bf3ab99854fd7c1
mellati laid <laidmellati@gmail.com>
2025-02-04 01:55:26 -0800

    created delete version cubit

--- lib/features/admin/materials/presentation/manager/delete_version_cubit/delete_version_cubit.dart
+++ lib/features/admin/materials/presentation/manager/delete_version_cubit/delete_version_cubit.dart
@@ -1,8 +1,24 @@
+import 'package:atm_app/features/admin/materials/domain/entities/aynaa_versions_entity.dart';
+import 'package:atm_app/features/admin/materials/domain/repos/versions_repo.dart';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'delete_version_state.dart';
 
 class DeleteVersionCubit extends Cubit<DeleteVersionState> {
-  DeleteVersionCubit() : super(DeleteVersionInitial());
+  final VersionsRepo versionsRepo;
+  DeleteVersionCubit({required this.versionsRepo})
+      : super(DeleteVersionInitial());
+
+  void deleteVersion({required AynaaVersionsEntity aynaaVersion}) async {
+    emit(DeleteVersionLoading());
+    final resault =
+        await versionsRepo.deleteVersion(aynaaVersion: aynaaVersion);
+
+    resault.fold((failure) {
+      emit(DeleteVersionFailed(failure.errMessage));
+    }, (lessons) {
+      emit(DeleteVersionSuccess());
+    });
+  }
 }
