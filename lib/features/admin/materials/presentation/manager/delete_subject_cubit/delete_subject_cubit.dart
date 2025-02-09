808cc095c86ae0e96d65c68cc1d484bb0fcc509c
mellati laid <laidmellati@gmail.com>
2025-02-09 02:14:50 -0800

    delete subject cubit done

--- lib/features/admin/materials/presentation/manager/delete_subject_cubit/delete_subject_cubit.dart
+++ lib/features/admin/materials/presentation/manager/delete_subject_cubit/delete_subject_cubit.dart
@@ -1,8 +1,18 @@
+import 'package:atm_app/core/services/delete_items_service.dart';
+import 'package:atm_app/features/admin/materials/domain/entities/deleted_itmes_entity.dart';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'delete_subject_state.dart';
 
 class DeleteSubjectCubit extends Cubit<DeleteSubjectState> {
-  DeleteSubjectCubit() : super(DeleteSubjectInitial());
+  final DeleteItemsService deleteItemsService;
+  DeleteSubjectCubit({required this.deleteItemsService})
+      : super(DeleteSubjectInitial());
+
+  Future<void> deleteSubject({required String id}) async {
+    emit(DeleteSubjectLoading());
+    final deletedItem = DeletedItmesEntity(id, false, false);
+    await deleteItemsService.markItemAsDeleted(item: deletedItem);
+  }
 }
