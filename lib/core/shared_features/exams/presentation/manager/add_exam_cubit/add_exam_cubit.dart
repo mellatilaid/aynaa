875ca094831baa4a4f79d0f6a71f99041b929223
mellati laid <laidmellati@gmail.com>
2025-02-16 02:36:33 -0800

    add exam cubit done

--- lib/core/shared_features/exams/presentation/manager/add_exam_cubit/add_exam_cubit.dart
+++ lib/core/shared_features/exams/presentation/manager/add_exam_cubit/add_exam_cubit.dart
@@ -1,8 +1,26 @@
+import 'package:atm_app/core/shared_features/exams/domain/entities/exam_entity.dart';
+import 'package:atm_app/core/shared_features/exams/domain/repos/exams_repo.dart';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'add_exam_state.dart';
 
 class AddExamCubit extends Cubit<AddExamState> {
-  AddExamCubit() : super(AddExamInitial());
+  final ExamsRepo examsRepo;
+  AddExamCubit({required this.examsRepo}) : super(AddExamInitial());
+
+  late String _filePath;
+  Future<void> addExam({required ExamEntity exam}) async {
+    emit(AddExamLoading());
+
+    final resault = await examsRepo.addExam(exam: exam, filePath: _filePath);
+
+    resault.fold((failure) {
+      emit(AddExamFailure(errMessage: failure.errMessage));
+    }, (succuss) {
+      emit(AddExamSuccuss());
+    });
+  }
+
+  void setFilePath(String filePath) => _filePath = filePath;
 }
