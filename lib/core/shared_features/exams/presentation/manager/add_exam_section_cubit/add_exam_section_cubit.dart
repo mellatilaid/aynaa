7aff792b5f821ed714c9f710372570f1f0fdf375
mellati laid <laidmellati@gmail.com>
2025-02-17 00:41:07 -0800

    add exam section cubit done

--- lib/core/shared_features/exams/presentation/manager/add_exam_section_cubit/add_exam_section_cubit.dart
+++ lib/core/shared_features/exams/presentation/manager/add_exam_section_cubit/add_exam_section_cubit.dart
@@ -1,8 +1,29 @@
+import 'package:atm_app/core/shared_features/exams/domain/entities/exam_sections_entity.dart';
+import 'package:atm_app/core/shared_features/exams/domain/repos/exam_sections_repo.dart';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'add_exam_section_state.dart';
 
 class AddExamSectionCubit extends Cubit<AddExamSectionState> {
-  AddExamSectionCubit() : super(AddExamSectionInitial());
+  final ExamSectionsRepo examSectionsRepo;
+  AddExamSectionCubit({required this.examSectionsRepo})
+      : super(AddExamSectionInitial());
+  late String _filePath;
+  Future<void> addExam({required ExamSectionsEntity examSection}) async {
+    emit(AddExamSectionLoading());
+
+    final resault = await examSectionsRepo.addExamSection(
+      examSection: examSection,
+      filePath: _filePath,
+    );
+
+    resault.fold((failure) {
+      emit(AddExamSectionFailure(errMessage: failure.errMessage));
+    }, (succuss) {
+      emit(AddExamSectionSuccess());
+    });
+  }
+
+  void setFilePath(String filePath) => _filePath = filePath;
 }
